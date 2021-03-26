b4847e15a263e2d9025d317efce8da4599dc033a
WahajMagazine341 <iwahajmubeen341@gmail.com>
2021-03-26 16:53:19 +0500

    environmental vars added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app= express();
 
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended:true}));
@@ -19,11 +21,12 @@ app.use('/posts', postRoutes);
 
 //www.mongodb.com/cloud/atlas
 
-const CONNECTION_URL= 'mongodb+srv://wahajmubeen:bohemia420@cluster0.bth3z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//const CONNECTION_URL= 'mongodb+srv://wahajmubeen:bohemia420@cluster0.bth3z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+console.log(process.env.CONNECTION_URL, process.env.PORT);
 
 const PORT= process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, ()=> console.log(`server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
